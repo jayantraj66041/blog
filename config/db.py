debc2c4d1e27f58effad0c416fcafa148b6811cd
Jayant Raj <jayantraj66041@gmail.com>
2023-11-18 12:32:33 +0530

    nosql query update

--- config/db.py
+++ config/db.py
@@ -3,4 +3,4 @@ from pymongo import MongoClient
 CONN_URL = "mongodb://localhost:27017/"
 DB_NAME = "learning1"
 
-conn = MongoClient(CONN_URL+DB_NAME)
+conn = MongoClient(CONN_URL+DB_NAME).learning1
--- routes/user.py
+++ routes/user.py
@@ -3,35 +3,38 @@ from models.user import User
 from config.db import conn
 from schemas.user import userEntity, usersEntity
 from bson import ObjectId
-from time import sleep
+
 
 user = APIRouter()
 
 
 @user.get("/")
 async def find_all_users():
-    return usersEntity(conn.learning1.user.find())
+    return usersEntity(conn.user.find())
 
 
 @user.get("/{id}")
 async def find_one_user(id:str):
-    return userEntity(conn.learning1.user.find_one({"_id": ObjectId(id)}))
+    return userEntity(conn.user.find_one({"_id": ObjectId(id)}))
+
 
 @user.post("/")
 async def create_user(user: User):
-    conn.learning1.user.insert_one(dict(user))
+    conn.user.insert_one(dict(user))
     return user
+
 
 @user.put("/{id}")
 async def update_user(id:str, user: User):
-    conn.learning1.user.find_one_and_update({"_id": ObjectId(id)}, {
+    conn.user.find_one_and_update({"_id": ObjectId(id)}, {
         "$set": dict(user)
     })
-    return userEntity(conn.learning1.user.find_one({"_id": ObjectId(id)}))
+    return userEntity(conn.user.find_one({"_id": ObjectId(id)}))
+
 
 @user.delete("/{id}")
 async def delete_user(id:str):
-    return userEntity(conn.learning1.user.find_one_and_delete({"_id": ObjectId(id)}))
+    return userEntity(conn.user.find_one_and_delete({"_id": ObjectId(id)}))
 
 
 websocket_list = []
